caf7a7d6c912c9d7b3dec14bb5b42c50968ea619
Matt Pickle <mpickle86@gmail.com>
2019-10-17 21:25:38 -0500

    Set up basic InputBox functionality

--- src/InputBox.js
+++ src/InputBox.js
@@ -1,18 +1,39 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 function InputBox() {
   const [todoItem, setTodoItem] = useState("");
-
+  
+  //initializes todoItemsArray from localStorage
+  const initialArrayString = localStorage.getItem("todoItemsArray");
+  const initialArray = JSON.parse(initialArrayString);
+  const [todoItemsArray, setTodoItemsArray] = useState(initialArray || []); 
 
   function handleChange(event) {
     setTodoItem(event.target.value);
   }
+
+  //adds new item to todoItemsArray in state
+  function handleSubmit(event) {
+    setTodoItemsArray(prevArray => prevArray.concat(todoItem));
+    setTodoItem("");
+    event.preventDefault();
+  }
+
+  //stores todoItemsArray in localStorage when new item is submitted
+  useEffect(() => {
+    localStorage.setItem("todoItemsArray", JSON.stringify(todoItemsArray));
+  }, [todoItemsArray]);
   
-  return <input type="text"
-                name="inputBox"
-                value={todoItem.value}
-                placeholder="New Todo Item"
-                onChange={handleChange} />
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text"
+             name="inputBox"
+             value={todoItem}
+             placeholder="New Todo Item"
+             onChange={handleChange} />
+      <button>Add</button>
+    </form>
+  );
 }
 
 export default InputBox;
