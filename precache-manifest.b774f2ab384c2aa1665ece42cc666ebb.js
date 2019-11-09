1703302c93570fd529a8ce2b4d759da6863507a0
Matt Pickle <mpickle86@gmail.com>
2019-11-08 21:34:03 -0600

    Updates

---
+++ precache-manifest.b774f2ab384c2aa1665ece42cc666ebb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f1aadf64d5a91b5d9b8596bd91898cc",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "8dc484a33ee26e4f4410",
+    "url": "/todo-list/static/css/main.7c4fe592.chunk.css"
+  },
+  {
+    "revision": "f0c9c6c068ca7d8b434a",
+    "url": "/todo-list/static/js/2.a235b894.chunk.js"
+  },
+  {
+    "revision": "8dc484a33ee26e4f4410",
+    "url": "/todo-list/static/js/main.f8a2a377.chunk.js"
+  },
+  {
+    "revision": "da715ee0b4f01398cebe",
+    "url": "/todo-list/static/js/runtime-main.14b3a6b5.js"
+  }
+]);
